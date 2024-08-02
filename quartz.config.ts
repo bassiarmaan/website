ee35e8bb24607c76717e57691876f240aa4c4957
Armaan Bassi <armalbassi@gmail.com>
2024-08-02 12:45:55 -0400

    Quartz sync: Aug 2, 2024, 12:45 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -28,7 +28,7 @@ const config: QuartzConfig = {
       },
       colors: {
         lightMode: {
-          light: "#8298B0", //background
+          light: "#8298b0", //background
           lightgray: "#e5e5e5",
           gray: "#b8b8b8",
           darkgray: "#4e4e4e", //text
